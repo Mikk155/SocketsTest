298bdbaa484f40c1c0d587a737dbbbc4a1c53b45
Mikk155 <martimsanchez155@gmail.com>
2025-08-03 18:13:49 -0300

    Add test executable to C++ Client

--- Client/CPlusPlus/CPlusPlus.cpp
+++ Client/CPlusPlus/CPlusPlus.cpp
@@ -1,7 +1,28 @@
+#include <windows.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    HMODULE dllHandle = LoadLibraryA( "Client.dll" );
+
+    if( !dllHandle )
+    {
+        std::cerr << "Failed to load Client.dll\n";
+        return 1;
+    }
+
+    using StartFunc = void(*)();
+    StartFunc StartClient = reinterpret_cast<StartFunc>( GetProcAddress( dllHandle, "StartClient" ) );
+
+    if( !StartClient )
+    {
+        std::cerr << "Failed to find method \"StartClient\"\n";
+        return 1;
+    }
+
+    StartClient();
+
+    FreeLibrary( dllHandle );
+
     return 0;
 }
---
+++ Client/CPlusPlus/main.cpp
@@ -0,0 +1,32 @@
+#include "SocketServer.h"
+
+std::atomic<bool> g_Running = true;
+
+void SimulateGameRunning()
+{
+    std::cin.get();
+    g_Running = false;
+}
+
+
+extern "C" __declspec( dllexport ) void StartClient()
+{
+    std::thread waitThread( SimulateGameRunning );
+
+    std::cout << "Press Enter at any moment to exit\n";
+
+    int time = 0;
+
+    while( g_Running )
+    {
+        std::this_thread::sleep_for( std::chrono::seconds(5) );
+
+        char msg[16];
+        sprintf( msg, "test %i", time );
+        g_Sockets.Send( msg );
+        time++;
+    }
+
+    g_Sockets.Shutdown();
+    waitThread.join();
+}
