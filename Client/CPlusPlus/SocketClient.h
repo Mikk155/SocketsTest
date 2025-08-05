26d5efc43cd5602d3859ac851daaa46715d4caaf
Mikk155 <martimsanchez155@gmail.com>
2025-08-05 12:31:30 -0300

    Improve C++ client

---
+++ Client/CPlusPlus/SocketClient.h
@@ -0,0 +1,263 @@
+/**
+ * @file SocketClient.h
+ * @author Mikk155
+ * @brief Implement a simple Client socket in its own thread to transfer and get information from a server
+ * @version 0.1
+ * @date 2025-08-04
+ * 
+ * @copyright Copyright (c) 2025 Mikk155
+ *
+ *    MIT License
+ *
+ *    Permission is hereby granted, free of charge, to any person obtaining a copy
+ *    of this software and associated documentation files (the "Software"), to deal
+ *    in the Software without restriction, including without limitation the rights
+ *    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the Software is
+ *    furnished to do so, subject to the following conditions:
+ *
+ *    The above copyright notice and this permission notice shall be included in all
+ *    copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *    SOFTWARE.
+*/
+
+#include <string>
+#include <iostream>
+#include <thread>
+#include <functional>
+#include <optional>
+#include <atomic>
+
+#pragma once
+
+#ifdef _WIN32
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+#pragma comment( lib, "ws2_32.lib" )
+#else
+// -🐧
+#endif
+#include <algorithm>
+
+class SocketClient
+{
+    using SocketCallback = std::function<void( const std::string& )>;
+
+    protected:
+
+        std::string Address;
+        int Port;
+        int RecBufferSize;
+        std::optional<SocketCallback> RecCallback;
+
+        std::atomic<bool> m_Online{false};
+
+        std::thread m_Thread;
+
+#ifdef _WIN32
+        SOCKET m_Socket = INVALID_SOCKET;
+#else
+// -🐧
+#endif
+
+    public:
+
+        void Setup( std::string _Address, int _Port, int _RecBufferSize, std::optional<SocketCallback> _RecCallback = std::nullopt )
+        {
+            Address = _Address;
+            Port = _Port;
+            RecBufferSize = std::clamp( _RecBufferSize, 1, SO_MAX_MSG_SIZE );
+            RecCallback = _RecCallback;
+
+            char buffer[256];
+            sprintf( buffer, "Setup at %s:%i with incoming buffer of %i", Address.c_str(), Port, RecBufferSize );
+            print( buffer );
+
+            if( !RecCallback.has_value() )
+            {
+                print( "No external callback method set. this client won't receive messages." );
+            }
+
+            TryConnect();
+        }
+
+        void Shutdown()
+        {
+            if( RecCallback.has_value() && m_Thread.joinable() && std::this_thread::get_id() != m_Thread.get_id() )
+            {
+                m_Thread.join();
+                m_Thread = std::thread();
+            }
+
+#ifdef _WIN32
+            if( IsActive() )
+            {
+                shutdown(m_Socket, SD_BOTH);
+                closesocket(m_Socket);
+                m_Socket = INVALID_SOCKET;
+            }
+
+            if( m_Online )
+            {
+                WSACleanup();
+                m_Online = false;
+            }
+#else
+// -🐧
+#endif
+        }
+
+        /**
+         * @brief Return whatever the client socket is currently active and has connection to the server
+         */
+        const bool IsActive()
+        {
+#ifdef _WIN32
+            return ( m_Socket != INVALID_SOCKET );
+#else
+// -🐧
+#endif
+        }
+
+        /**
+         * @brief Send a message to the server socket. if we're not connected it will attempt to connect first.
+         */
+        void Send( const char* message )
+        {
+            if( message == nullptr || message[0] == '\0' )
+                return;
+
+            if( !IsActive() )
+            {
+                TryConnect();
+            }
+
+            if( !IsActive() )
+                return;
+
+#ifdef _WIN32
+            int result = send( m_Socket, message, static_cast<int>( strlen( message ) ), 0 );
+
+            if( result == SOCKET_ERROR )
+            {
+                int error = WSAGetLastError();
+
+                char buffer[256];
+                sprintf( buffer, "Error sending string: %i for message:", error );
+                print( buffer );
+                print( message );
+
+                Shutdown();
+            }
+#else
+// -🐧
+#endif
+        }
+
+        /**
+         * @brief Send a message to the server socket. if we're not connected it will attempt to connect first.
+         */
+        void Send( const std::string& message )
+        {
+            if( !message.empty() )
+            {
+                Send( message.c_str() );
+            }
+        }
+
+        /**
+         * @brief Shutdown connection if any and attempt to connect
+         */
+        void TryConnect()
+        {
+            Shutdown();
+
+#ifdef _WIN32
+            WSADATA wsa;
+
+            if( WSAStartup( MAKEWORD( 2, 2 ), &wsa ) != 0 )
+            {
+                print( "WSAStartup failed." );
+                return;
+            }
+
+            m_Online = true;
+
+            m_Socket = socket( AF_INET, SOCK_STREAM, 0 );
+
+            if( !IsActive() )
+            {
+                print( "Socket creation failed." );
+                Shutdown();
+                return;
+            }
+
+            sockaddr_in server;
+            server.sin_family = AF_INET;
+            server.sin_port = htons( Port );
+            server.sin_addr.s_addr = inet_addr( Address.c_str() );
+
+            if( connect( m_Socket, (sockaddr*)&server, sizeof( server ) ) < 0 )
+            {
+                print( "Failed to connect to a server" );
+                Shutdown();
+                return;
+            }
+#else
+// -🐧
+#endif
+            print( "Connected to a server" );
+
+            if( RecCallback.has_value() )
+            {
+                if( m_Thread.joinable() )
+                {
+                    m_Thread.join();
+                }
+
+                m_Thread = std::thread( &SocketClient::SocketThread, this );
+            }
+        }
+
+    protected:
+
+        void SocketThread()
+        {
+            char buffer[SO_MAX_MSG_SIZE];
+
+            while( IsActive() )
+            {
+#ifdef _WIN32
+                if( int bytes = recv( m_Socket, buffer, RecBufferSize, 0 ); bytes > 0 )
+                {
+                    std::string msg( buffer, bytes );
+
+                    RecCallback.value()(msg);
+                }
+                else
+                {
+                    closesocket(m_Socket);
+                    m_Socket = INVALID_SOCKET;
+                    break;
+                }
+#else
+// -🐧
+#endif
+            }
+        }
+
+        /**
+         * @brief Print a message to console, overide method to print to the game message system
+         */
+        void print( const char* message )
+        {
+            std::cout << "[SocketClient] " << message << std::endl;
+        }
+};
--- Client/CPlusPlus/main.cpp
+++ Client/CPlusPlus/main.cpp
@@ -1,37 +1,88 @@
-#include "SocketServer.h"
-#include <atomic>
+/**
+ *    MIT License
+ *
+ *    Copyright (c) 2025 Mikk155
+ *
+ *    Permission is hereby granted, free of charge, to any person obtaining a copy
+ *    of this software and associated documentation files (the "Software"), to deal
+ *    in the Software without restriction, including without limitation the rights
+ *    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *    copies of the Software, and to permit persons to whom the Software is
+ *    furnished to do so, subject to the following conditions:
+ *
+ *    The above copyright notice and this permission notice shall be included in all
+ *    copies or substantial portions of the Software.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *    SOFTWARE.
+*/
 
-std::atomic<bool> g_Running = true;
+#include "SocketClient.h"
 
-void SimulateGameRunning()
+#include <csignal>
+
+// Simulate a game server running syncronously
+std::atomic<bool> g_GameServerRunning;
+
+inline SocketClient g_Sockets;
+
+void MyMessageHandler( const std::string& message )
 {
-    std::cin.get();
-    g_Running = false;
+    std::cout << message << std::endl;;
 }
 
 int main()
 {
-    std::thread waitThread( SimulateGameRunning );
+    g_Sockets.Setup( "127.0.0.1", 5000, 128, MyMessageHandler );
 
-    std::cout << "Press Enter at any moment to exit\n";
-
-    g_Sockets.Send( "Hello world from the game server!\n" );
-
-    int time = 0;
-
-    while( g_Running )
+    // Have a thread for testing a simulation of game messages
+    std::thread WaitForUserInputToExit( []()
     {
-        std::this_thread::sleep_for( std::chrono::seconds(5) );
+        std::cout << "Type 'quit' and press Enter to exit the program at any moment.\n";
 
-        continue;
+        std::string line;
 
-        char msg[GLDSOURCE_CHAT_MAX_CHARS];
-        sprintf( msg, "test %i\n", time );
-        g_Sockets.Send( msg );
-        time++;
+        while( std::getline( std::cin, line ) )
+        {
+            if( line == "quit" )
+            {
+                g_GameServerRunning = false;
+                break;
+            }
+            else
+            {
+                g_Sockets.Send( line.c_str() );
+            }
+        }
+    } );
+
+    auto ExitProgram = []( int signal )
+    {
+        g_GameServerRunning = false;
+    };
+
+    std::signal( SIGINT, ExitProgram );
+    std::signal( SIGTERM, ExitProgram );
+
+    SetConsoleCtrlHandler( []( DWORD type )
+    {
+        g_GameServerRunning = false;
+        return TRUE;
+    }, TRUE );
+
+    while( g_GameServerRunning )
+    {
+        std::this_thread::sleep_for( std::chrono::milliseconds(100) );
     }
 
+    WaitForUserInputToExit.join();
+
     g_Sockets.Shutdown();
-    waitThread.join();
+
     return 0;
 }
