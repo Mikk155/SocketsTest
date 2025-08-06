94bb1be728855ed8a2245fa0633fdbfa18ebee19
Mikk155 <martimsanchez155@gmail.com>
2025-08-06 10:38:22 -0300

    send server status

--- Client/CPlusPlus/main.cpp
+++ Client/CPlusPlus/main.cpp
@@ -40,6 +40,22 @@ void MyMessageHandler( const std::string& message )
 
 using json = nlohmann::json;
 
+struct CBasePlayer
+{
+    int dead;
+    std::string steam;
+    float frags;
+    std::string name;
+
+    CBasePlayer( int _dead, std::string _steam, float _frags, std::string _name )
+    {
+        dead = _dead;
+        steam = _steam;
+        frags = _frags;
+        name = _name;
+    }
+};
+
 int main()
 {
     g_Sockets.Setup( "127.0.0.1", 5000, 128, MyMessageHandler );
@@ -58,6 +74,48 @@ int main()
                 std::lock_guard<std::mutex> lock(_STOP_GAME);
                 g_GameServerRunning = false;
                 break;
+            }
+            if( line == "status" )
+            {
+                json status = json::object();
+                status[ "hostname" ] = "Dedicated Server";
+                status[ "channel" ] = 1401679321776787516;
+                status[ "mapname" ] = "hl_c04";
+                status[ "nextmap" ] = "hl_c05_a1";
+
+                json players_data = json::array();
+
+                std::vector<CBasePlayer> players =
+                {
+                    CBasePlayer( 0, "STEAMID_1", 1.5, "Mikk" ),
+                    CBasePlayer( 1, "STEAMID_2", 0.0, "Gafther" ),
+                    CBasePlayer( 2, "STEAMID_3", 10.0, "Zode" )
+            /*
+                    error C7555: el uso de los inicializadores designados requiere al menos "/std:c++20" :ExtremeRage:
+                    CBasePlayer{ .dead = 0, .steam = "STEAMID_1", .frags = 1.5, .name = "Mikk" },
+                    CBasePlayer{ .dead = 1, .steam = "STEAMID_2", .frags = 0.0, .name = "Gafther" },
+                    CBasePlayer{ .dead = 2, .steam = "STEAMID_3", .frags = 10.0, .name = "Zode" }
+            */
+                };
+
+                for( const CBasePlayer& p : players )
+                {
+                    json player_data = json::object();
+
+                    player_data[ "name" ] = p.name;
+                    player_data[ "dead" ] = p.dead;
+                    player_data[ "steam" ] = p.steam;
+                    player_data[ "frags" ] = (int)p.frags;
+
+                    players_data.emplace_back( player_data );
+                }
+
+                status[ "players" ] = players_data;
+
+                std::string jobject = "```json\n" + status.dump(4) + "\n```";
+
+                g_Sockets.Send( jobject );
+                g_Sockets.Send( status.dump() );
             }
             else
             {
@@ -82,7 +140,7 @@ int main()
 
     while( g_GameServerRunning )
     {
-        std::this_thread::sleep_for( std::chrono::milliseconds(100) );
+        std::this_thread::sleep_for( std::chrono::seconds(1) );
     }
 
     WaitForUserInputToExit.join();
